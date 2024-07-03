f034de155ea2679209314a74efcb64479b029c3c
FelipeOliveira22 <felipe.olv@alu.ufc.br>
2024-07-03 15:44:59 -0300

    Atualização
    Signed-off-by: FelipeOliveira22 <felipe.olv@alu.ufc.br>

--- lab-banco-digital-oo-master/src/Main.java
+++ lab-banco-digital-oo-master/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente felipe = new Cliente();
+		felipe.setNome("Felipe");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(felipe);
+		Conta poupanca = new ContaPoupanca(felipe);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
